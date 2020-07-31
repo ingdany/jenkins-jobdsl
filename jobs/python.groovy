7e11e5f76131d73506f202f7027f3a0b7afad428
Daniel Perez <ing.dany@gmail.com>
2020-07-30 17:27:42 -0700

    Update groovy, add cpsCsm

--- jobs/python.groovy
+++ jobs/python.groovy
@@ -3,13 +3,17 @@ repo = "https://github.com/ingdany/jenkins-jobdsl.git"
 repo_name = "repo"
 
 pipelineJob(project_name) {
-    scm {
-        git {
-            remote {
-                name(repo_name)
-                url(repo)
-            }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        name(repo_name)
+                        url(repo)
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            } 
         }
-        scriptPath("Jenkinsfile")
-    }    
+    }
 }
